7909d927b42cfab92bcb19925f6ddb8613ea266c
along <1746017379@qq.com>
2021-02-07 21:57:42 +0800

    新增分支，新增方法

--- src/main/java/com/example/apollo/controller/UserController.java
+++ src/main/java/com/example/apollo/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.example.apollo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author DongChengLong
@@ -21,11 +19,27 @@ public class UserController {
     @Value("${server.user.name}")
     private String name;
 
-    @GetMapping(value = "/user")
+    @ResponseBody
+    @GetMapping(value = "/hello")
     public String user(){
         log.info("从配置文件获取的值为：{}", name);
         return "Hello SpringBootApollo：" + name;
     }
 
+
+    @ResponseBody
+    @PostMapping(value = "/sum")
+    public int sum(@RequestBody JSONObject jsonObject){
+        Integer num1 = null;
+        Integer num2 = null;
+        try {
+            num1 = jsonObject.getInteger("num1");
+            num2 = jsonObject.getInteger("num2");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("JSONObject 类型类型转换异常：{}", e.getMessage());
+        }
+        return num1 + num2;
+    }
 
 }
