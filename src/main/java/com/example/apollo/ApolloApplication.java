9ae68aa65f585903dea15fe4bd98c04d38971068
zhangwenlong <2364109364@qq.com>
2021-02-08 11:38:40 +0800

    2021-02-08 z.w.l add 清除启动类无用输出

--- src/main/java/com/example/apollo/ApolloApplication.java
+++ src/main/java/com/example/apollo/ApolloApplication.java
@@ -8,11 +8,6 @@ public class ApolloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApolloApplication.class, args);
-        System.out.println("55");
-        //2021-02-08 z.w.l add 测试 分支
-        System.out.println("z.w.l");
-        //2021-02-08 z.w.l end 测试 分支
     }
 
-    
 }
