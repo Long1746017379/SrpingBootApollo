f0fdc50eaaca0899e2524c9d072637c3fce52514
along <1746017379@qq.com>
2021-02-08 15:46:33 +0800

    新增Dao，查询数据库

--- src/main/java/com/example/apollo/ApolloApplication.java
+++ src/main/java/com/example/apollo/ApolloApplication.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author DongChengLong
+ * @desc SpringBoot启动类
+ * @date 2021/01/13 20:47
+ */
 @MapperScan("com.example.apollo.dao")
 @SpringBootApplication
 public class ApolloApplication {
