55fae627c582aa23be3a80759d4752081ef24d8a
along <1746017379@qq.com>
2021-03-13 00:09:50 +0800

    新增启动类

---
+++ src/main/java/com/example/apollo/config/InitEngineerConfig.java
@@ -0,0 +1,14 @@
+package com.example.apollo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author DongChengLong
+ * @desc
+ * @date 2021/03/13 0:05
+ */
+@Configuration
+public class InitEngineerConfig {
+
+
+}
